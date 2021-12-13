bfff3ba8e309818df53b42c42e9e46b7e2758379
ranaya18 <70286070+ranaya18@users.noreply.github.com>
2021-12-13 01:16:28 -0800

    cards

--- js/lab.js
+++ js/lab.js
@@ -16,12 +16,14 @@
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".aries-card").click(function(){
-      $("#aries-info").fadeIn();
-      $("#aries-info").fadeIn("slow");
-
-      $("#aries-info1").fadeIn();
-      $("#aries-info1").fadeIn("slow");
-
+      $("#aries-info").slideToggle();
+      $("#aries-info").slideToggle("slow");
+      $("#aries-info1").slideToggle();
+      $("#aries-info1").slideToggle("slow");
+      $("#aries-info2").slideToggle();
+      $("#aries-info2").slideToggle("slow");
+      $("#aries-info3").slideToggle();
+      $("#aries-info3").slideToggle("slow");
         $("#aries-info").toggle();
         $("#aries-info1").toggle();
         $("#aries-info2").toggle();
@@ -33,12 +35,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".taurus-card").click(function(){
-      $("#taurus-info").fadeIn();
-      $("#taurus-info").fadeIn("slow");
-      $("#taurus-info").fadeIn(3000);
-      $("#taurus-info1").fadeIn();
-      $("#taurus-info1").fadeIn("slow");
-      $("#taurus-info1").fadeIn(3000);
+      $("#taurus-info").slideToggle();
+      $("#taurus-info").slideToggle("slow");
+      $("#taurus-info1").slideToggle();
+      $("#taurus-info1").slideToggle("slow");
+      $("#taurus-info2").slideToggle();
+      $("#taurus-info2").slideToggle("slow");
+      $("#taurus-info3").slideToggle();
+      $("#taurus-info3").slideToggle("slow");
         $("#taurus-info").toggle();
         $("#taurus-info1").toggle();
         $("#taurus-info2").toggle();
@@ -49,18 +53,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".gemini-card").click(function(){
-      $("#gemini-info").fadeIn();
-      $("#gemini-info").fadeIn("slow");
-      $("#gemini-info").fadeIn(3000);
-      $("#gemini-info1").fadeIn();
-      $("#gemini-info1").fadeIn("slow");
-      $("#gemini-info1").fadeIn(3000);
-      $("#gemini-info2").fadeIn();
-      $("#gemini-info2").fadeIn("slow");
-      $("#gemini-info2").fadeIn(3000);
-      $("#gemini-info3").fadeIn();
-      $("#gemini-info3").fadeIn("slow");
-      $("#gemini-info3").fadeIn(3000);
+      $("#gemini-info").slideToggle();
+      $("#gemini-info").slideToggle("slow");
+      $("#gemini-info1").slideToggle();
+      $("#gemini-info1").slideToggle("slow");
+      $("#gemini-info2").slideToggle();
+      $("#gemini-info2").slideToggle("slow");
+      $("#gemini-info3").slideToggle();
+      $("#gemini-info3").slideToggle("slow");
         $("#gemini-info").toggle();
         $("#gemini-info1").toggle();
         $("#gemini-info2").toggle();
@@ -72,12 +72,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".cancer-card").click(function(){
-      $("#cancer-info").fadeIn();
-      $("#cancer-info").fadeIn("slow");
-      $("#cancer-info").fadeIn(3000);
-      $("#cancer-info1").fadeIn();
-      $("#cancer-info1").fadeIn("slow");
-      $("#cancer-info1").fadeIn(3000);
+      $("#cancer-info").slideToggle();
+      $("#cancer-info").slideToggle("slow");
+      $("#cancer-info1").slideToggle();
+      $("#cancer-info1").slideToggle("slow");
+      $("#cancer-info2").slideToggle();
+      $("#cancer-info2").slideToggle("slow");
+      $("#cancer-info3").slideToggle();
+      $("#cancer-info3").slideToggle("slow");
         $("#cancer-info").toggle();
         $("#cancer-info1").toggle();
         $("#cancer-info2").toggle();
@@ -89,12 +91,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".leo-card").click(function(){
-      $("#leo-info").fadeIn();
-      $("#leo-info").fadeIn("slow");
-      $("#leo-info").fadeIn(3000);
-      $("#leo-info1").fadeIn();
-      $("#leo-info1").fadeIn("slow");
-      $("#leo-info1").fadeIn(3000);
+      $("#leo-info").slideToggle();
+      $("#leo-info").slideToggle("slow");
+      $("#leo-info1").slideToggle();
+      $("#leo-info1").slideToggle("slow");
+      $("#leo-info2").slideToggle();
+      $("#leo-info2").slideToggle("slow");
+      $("#leo-info3").slideToggle();
+      $("#leo-info3").slideToggle("slow");
         $("#leo-info").toggle();
         $("#leo-info1").toggle();
         $("#leo-info2").toggle();
@@ -106,12 +110,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".virgo-card").click(function(){
-      $("#virgo-info").fadeIn();
-      $("#virgo-info").fadeIn("slow");
-      $("#virgo-info").fadeIn(3000);
-      $("#virgo-info1").fadeIn();
-      $("#virgo-info1").fadeIn("slow");
-      $("#virgo-info1").fadeIn(3000);
+      $("#virgo-info").slideToggle();
+      $("#virgo-info").slideToggle("slow");
+      $("#virgo-info1").slideToggle();
+      $("#virgo-info1").slideToggle("slow");
+      $("#virgo-info2").slideToggle();
+      $("#virgo-info2").slideToggle("slow");
+      $("#virgo-info3").slideToggle();
+      $("#virgo-info3").slideToggle("slow");
         $("#virgo-info").toggle();
         $("#virgo-info1").toggle();
         $("#virgo-info2").toggle();
@@ -123,12 +129,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".libra-card").click(function(){
-      $("#libra-info").fadeIn();
-      $("#libra-info").fadeIn("slow");
-      $("#libra-info").fadeIn(3000);
-      $("#libra-info1").fadeIn();
-      $("#libra-info1").fadeIn("slow");
-      $("#libra-info1").fadeIn(3000);
+      $("#libra-info").slideToggle();
+      $("#libra-info").slideToggle("slow");
+      $("#libra-info1").slideToggle();
+      $("#libra-info1").slideToggle("slow");
+      $("#libra-info2").slideToggle();
+      $("#libra-info2").slideToggle("slow");
+      $("#libra-info3").slideToggle();
+      $("#libra-info3").slideToggle("slow");
         $("#libra-info").toggle();
         $("#libra-info1").toggle();
         $("#libra-info2").toggle();
@@ -140,12 +148,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".scorpio-card").click(function(){
-      $("#scorpio-info").fadeIn();
-      $("#scorpio-info").fadeIn("slow");
-      $("#scorpio-info").fadeIn(3000);
-      $("#scorpio-info1").fadeIn();
-      $("#scorpio-info1").fadeIn("slow");
-      $("#scorpio-info1").fadeIn(3000);
+      $("#scorpio-info").slideToggle();
+      $("#scorpio-info").slideToggle("slow");
+      $("#scorpio-info1").slideToggle();
+      $("#scorpio-info1").slideToggle("slow");
+      $("#scorpio-info2").slideToggle();
+      $("#scorpio-info2").slideToggle("slow");
+      $("#scorpio-info3").slideToggle();
+      $("#scorpio-info3").slideToggle("slow");
         $("#scorpio-info").toggle();
         $("#scorpio-info1").toggle();
         $("#scorpio-info2").toggle();
@@ -157,12 +167,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".sagittarius-card").click(function(){
-      $("#sagittarius-info").fadeIn();
-      $("#sagittarius-info").fadeIn("slow");
-      $("#sagittarius-info").fadeIn(3000);
-      $("#sagittarius-info1").fadeIn();
-      $("#sagittarius-info1").fadeIn("slow");
-      $("#sagittarius-info1").fadeIn(3000);
+      $("#sagittarius-info").slideToggle();
+      $("#sagittarius-info").slideToggle("slow");
+      $("#sagittarius-info1").slideToggle();
+      $("#sagittarius-info1").slideToggle("slow");
+      $("#sagittarius-info2").slideToggle();
+      $("#sagittarius-info2").slideToggle("slow");
+      $("#sagittarius-info3").slideToggle();
+      $("#sagittarius-info3").slideToggle("slow");
         $("#sagittarius-info").toggle();
         $("#sagittarius-info1").toggle();
         $("#sagittarius-info2").toggle();
@@ -174,12 +186,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".capricorn-card").click(function(){
-      $("#capricorn-info").fadeIn();
-      $("#capricorn-info").fadeIn("slow");
-      $("#capricorn-info").fadeIn(3000);
-      $("#capricorn-info1").fadeIn();
-      $("#capricorn-info1").fadeIn("slow");
-      $("#capricorn-info1").fadeIn(3000);
+      $("#capricorn-info").slideToggle();
+      $("#capricorn-info").slideToggle("slow");
+      $("#capricorn-info1").slideToggle();
+      $("#capricorn-info1").slideToggle("slow");
+      $("#capricorn-info2").slideToggle();
+      $("#capricorn-info2").slideToggle("slow");
+      $("#capricorn-info3").slideToggle();
+      $("#capricorn-info3").slideToggle("slow");
         $("#capricorn-info").toggle();
         $("#capricorn-info1").toggle();
         $("#capricorn-info2").toggle();
@@ -191,12 +205,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".aquarius-card").click(function(){
-      $("#aquarius-info").fadeIn();
-      $("#aquarius-info").fadeIn("slow");
-      $("#aquarius-info").fadeIn(3000);
-      $("#aquarius-info1").fadeIn();
-      $("#aquarius-info1").fadeIn("slow");
-      $("#aquarius-info1").fadeIn(3000);
+      $("#aquarius-info").slideToggle();
+      $("#aquarius-info").slideToggle("slow");
+      $("#aquarius-info1").slideToggle();
+      $("#aquarius-info1").slideToggle("slow");
+      $("#aquarius-info2").slideToggle();
+      $("#aquarius-info2").slideToggle("slow");
+      $("#aquarius-info3").slideToggle();
+      $("#aquarius-info3").slideToggle("slow");
         $("#aquarius-info").toggle();
         $("#aquarius-info1").toggle();
         $("#aquarius-info2").toggle();
@@ -208,12 +224,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".pisces-card").click(function(){
-      $("#pisces-info").fadeIn();
-      $("#pisces-info").fadeIn("slow");
-      $("#pisces-info").fadeIn(3000);
-      $("#pisces-info1").fadeIn();
-      $("#pisces-info1").fadeIn("slow");
-      $("#pisces-info1").fadeIn(3000);
+      $("#pisces-info").slideToggle();
+      $("#pisces-info").slideToggle("slow");
+      $("#pisces-info1").slideToggle();
+      $("#pisces-info1").slideToggle("slow");
+      $("#pisces-info2").slideToggle();
+      $("#pisces-info2").slideToggle("slow");
+      $("#pisces-info3").slideToggle();
+      $("#pisces-info3").slideToggle("slow");
         $("#pisces-info").toggle();
         $("#pisces-info1").toggle();
         $("#pisces-info2").toggle();
@@ -225,130 +243,119 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-aries-btn").click(function(){
-        $("#aries-info").toggle();
-        $("#aries-info1").toggle();
-        $("#aries-info2").toggle();
-        $("#aries-info3").toggle();
-        $("#aries-info4").toggle();
+         $("#aries-info").slideToggle("slow");
+          $("#aries-info1").slideToggle("slow");
+          $("#aries-info2").slideToggle("slow");
+          $("#aries-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-taurus-btn").click(function(){
-        $("#taurus-info").toggle();
-        $("#taurus-info1").toggle();
-        $("#taurus-info2").toggle();
-        $("#taurus-info3").toggle();
+      $("#taurus-info").slideToggle("slow");
+      $("#taurus-info1").slideToggle("slow");
+      $("#taurus-info2").slideToggle("slow");
+      $("#taurus-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-gemini-btn").click(function(){
-        $("#gemini-info").toggle();
-        $("#gemini-info1").toggle();
-        $("#gemini-info2").toggle();
-        $("#gemini-info3").toggle();
-        $("#gemini-info4").toggle();
+      $("#gemini-info").slideToggle("slow");
+      $("#gemini-info1").slideToggle("slow");
+      $("#gemini-info2").slideToggle("slow");
+      $("#gemini-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-cancer-btn").click(function(){
-        $("#cancer-info").toggle();
-        $("#cancer-info1").toggle();
-        $("#cancer-info2").toggle();
-        $("#cancer-info3").toggle();
-        $("#cancer-info4").toggle();
+      $("#cancer-info").slideToggle("slow");
+      $("#cancer-info1").slideToggle("slow");
+      $("#cancer-info2").slideToggle("slow");
+      $("#cancer-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-leo-btn").click(function(){
-        $("#leo-info").toggle();
-        $("#leo-info1").toggle();
-        $("#leo-info2").toggle();
-        $("#leo-info3").toggle();
-        $("#leo-info4").toggle();
+      $("#leo-info").slideToggle("slow");
+      $("#leo-info1").slideToggle("slow");
+      $("#leo-info2").slideToggle("slow");
+      $("#leo-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-virgo-btn").click(function(){
-        $("#virgo-info").toggle();
-        $("#virgo-info1").toggle();
-        $("#virgo-info2").toggle();
-        $("#virgo-info3").toggle();
-        $("#virgo-info4").toggle();
+      $("#virgo-info").slideToggle("slow");
+      $("#virgo-info1").slideToggle("slow");
+      $("#virgo-info2").slideToggle("slow");
+      $("#virgo-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-libra-btn").click(function(){
-        $("#libra-info").toggle();
-        $("#libra-info1").toggle();
-        $("#libra-info2").toggle();
-        $("#libra-info3").toggle();
-        $("#libra-info4").toggle();
+      $("#libra-info").slideToggle("slow");
+      $("#libra-info1").slideToggle("slow");
+      $("#libra-info2").slideToggle("slow");
+      $("#libra-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-scorpio-btn").click(function(){
-        $("#scorpio-info").toggle();
-        $("#scorpio-info1").toggle();
-        $("#scorpio-info2").toggle();
-        $("#scorpio-info3").toggle();
-        $("#scorpio-info4").toggle();
+      $("#scorpio-info").slideToggle("slow");
+      $("#scorpio-info1").slideToggle("slow");
+      $("#scorpio-info2").slideToggle("slow");
+      $("#scorpio-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-sagittarius-btn").click(function(){
-        $("#sagittarius-info").toggle();
-        $("#sagittarius-info1").toggle();
-        $("#sagittarius-info2").toggle();
-        $("#sagittarius-info3").toggle();
-        $("#sagittarius-info4").toggle();
+      $("#sagittarius-info").slideToggle("slow");
+      $("#sagittarius-info1").slideToggle("slow");
+      $("#sagittarius-info2").slideToggle("slow");
+      $("#sagittarius-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-capricorn-btn").click(function(){
-        $("#capricorn-info").toggle();
-        $("#capricorn-info1").toggle();
-        $("#capricorn-info2").toggle();
-        $("#capricorn-info3").toggle();
-        $("#capricorn-info4").toggle();
+      $("#capricorn-info").slideToggle("slow");
+      $("#capricorn-info1").slideToggle("slow");
+      $("#capricorn-info2").slideToggle("slow");
+      $("#capricorn-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-aquarius-btn").click(function(){
-        $("#aquarius-info").toggle();
-        $("#aquarius-info1").toggle();
-        $("#aquarius-info2").toggle();
-        $("#aquarius-info3").toggle();
-        $("#aquarius-info4").toggle();
+      $("#aquarius-info").slideToggle("slow");
+      $("#aquarius-info1").slideToggle("slow");
+      $("#aquarius-info2").slideToggle("slow");
+      $("#aquarius-info3").slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".toggle-pisces-btn").click(function(){
-        $("#pisces-info").toggle();
-        $("#pisces-info1").toggle();
-        $("#pisces-info2").toggle();
-        $("#pisces-info3").toggle();
-        $("#pisces-info4").toggle();
+      $("#pisces-info").slideToggle("slow");
+      $("#pisces-info1").slideToggle("slow");
+      $("#pisces-info2").slideToggle("slow");
+      $("#pisces-info3").slideToggle("slow");
     });
 });
