88636dd35edef4c78ae2aa59f28686eee340c502
ranaya18 <70286070+ranaya18@users.noreply.github.com>
2021-12-13 00:02:27 -0800

    background

--- js/lab.js
+++ js/lab.js
@@ -33,6 +33,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".taurus-card").click(function(){
+      $("#taurus-info").fadeIn();
+      $("#taurus-info").fadeIn("slow");
+      $("#taurus-info").fadeIn(3000);
+      $("#taurus-info1").fadeIn();
+      $("#taurus-info1").fadeIn("slow");
+      $("#taurus-info1").fadeIn(3000);
         $("#taurus-info").toggle();
         $("#taurus-info1").toggle();
         $("#taurus-info2").toggle();
@@ -43,6 +49,18 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".gemini-card").click(function(){
+      $("#gemini-info").fadeIn();
+      $("#gemini-info").fadeIn("slow");
+      $("#gemini-info").fadeIn(3000);
+      $("#gemini-info1").fadeIn();
+      $("#gemini-info1").fadeIn("slow");
+      $("#gemini-info1").fadeIn(3000);
+      $("#gemini-info2").fadeIn();
+      $("#gemini-info2").fadeIn("slow");
+      $("#gemini-info2").fadeIn(3000);
+      $("#gemini-info3").fadeIn();
+      $("#gemini-info3").fadeIn("slow");
+      $("#gemini-info3").fadeIn(3000);
         $("#gemini-info").toggle();
         $("#gemini-info1").toggle();
         $("#gemini-info2").toggle();
@@ -54,6 +72,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".cancer-card").click(function(){
+      $("#cancer-info").fadeIn();
+      $("#cancer-info").fadeIn("slow");
+      $("#cancer-info").fadeIn(3000);
+      $("#cancer-info1").fadeIn();
+      $("#cancer-info1").fadeIn("slow");
+      $("#cancer-info1").fadeIn(3000);
         $("#cancer-info").toggle();
         $("#cancer-info1").toggle();
         $("#cancer-info2").toggle();
@@ -65,6 +89,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".leo-card").click(function(){
+      $("#leo-info").fadeIn();
+      $("#leo-info").fadeIn("slow");
+      $("#leo-info").fadeIn(3000);
+      $("#leo-info1").fadeIn();
+      $("#leo-info1").fadeIn("slow");
+      $("#leo-info1").fadeIn(3000);
         $("#leo-info").toggle();
         $("#leo-info1").toggle();
         $("#leo-info2").toggle();
@@ -76,6 +106,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".virgo-card").click(function(){
+      $("#virgo-info").fadeIn();
+      $("#virgo-info").fadeIn("slow");
+      $("#virgo-info").fadeIn(3000);
+      $("#virgo-info1").fadeIn();
+      $("#virgo-info1").fadeIn("slow");
+      $("#virgo-info1").fadeIn(3000);
         $("#virgo-info").toggle();
         $("#virgo-info1").toggle();
         $("#virgo-info2").toggle();
@@ -87,6 +123,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".libra-card").click(function(){
+      $("#libra-info").fadeIn();
+      $("#libra-info").fadeIn("slow");
+      $("#libra-info").fadeIn(3000);
+      $("#libra-info1").fadeIn();
+      $("#libra-info1").fadeIn("slow");
+      $("#libra-info1").fadeIn(3000);
         $("#libra-info").toggle();
         $("#libra-info1").toggle();
         $("#libra-info2").toggle();
@@ -98,6 +140,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".scorpio-card").click(function(){
+      $("#scorpio-info").fadeIn();
+      $("#scorpio-info").fadeIn("slow");
+      $("#scorpio-info").fadeIn(3000);
+      $("#scorpio-info1").fadeIn();
+      $("#scorpio-info1").fadeIn("slow");
+      $("#scorpio-info1").fadeIn(3000);
         $("#scorpio-info").toggle();
         $("#scorpio-info1").toggle();
         $("#scorpio-info2").toggle();
@@ -109,6 +157,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".sagittarius-card").click(function(){
+      $("#sagittarius-info").fadeIn();
+      $("#sagittarius-info").fadeIn("slow");
+      $("#sagittarius-info").fadeIn(3000);
+      $("#sagittarius-info1").fadeIn();
+      $("#sagittarius-info1").fadeIn("slow");
+      $("#sagittarius-info1").fadeIn(3000);
         $("#sagittarius-info").toggle();
         $("#sagittarius-info1").toggle();
         $("#sagittarius-info2").toggle();
@@ -120,6 +174,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".capricorn-card").click(function(){
+      $("#capricorn-info").fadeIn();
+      $("#capricorn-info").fadeIn("slow");
+      $("#capricorn-info").fadeIn(3000);
+      $("#capricorn-info1").fadeIn();
+      $("#capricorn-info1").fadeIn("slow");
+      $("#capricorn-info1").fadeIn(3000);
         $("#capricorn-info").toggle();
         $("#capricorn-info1").toggle();
         $("#capricorn-info2").toggle();
@@ -131,6 +191,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".aquarius-card").click(function(){
+      $("#aquarius-info").fadeIn();
+      $("#aquarius-info").fadeIn("slow");
+      $("#aquarius-info").fadeIn(3000);
+      $("#aquarius-info1").fadeIn();
+      $("#aquarius-info1").fadeIn("slow");
+      $("#aquarius-info1").fadeIn(3000);
         $("#aquarius-info").toggle();
         $("#aquarius-info1").toggle();
         $("#aquarius-info2").toggle();
@@ -142,6 +208,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     // Toggles paragraphs display
     $(".pisces-card").click(function(){
+      $("#pisces-info").fadeIn();
+      $("#pisces-info").fadeIn("slow");
+      $("#pisces-info").fadeIn(3000);
+      $("#pisces-info1").fadeIn();
+      $("#pisces-info1").fadeIn("slow");
+      $("#pisces-info1").fadeIn(3000);
         $("#pisces-info").toggle();
         $("#pisces-info1").toggle();
         $("#pisces-info2").toggle();
